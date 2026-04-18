10b3d938687d8a4cc69dd97c788af96398629a8e
James Hodgkinson <james@terminaloutcomes.com>
2026-04-18 11:19:08 +1000

    things

--- tools/ble_sniffer/extcap/nrf_sniffer_ble.sh
+++ tools/ble_sniffer/extcap/nrf_sniffer_ble.sh
@@ -2,9 +2,9 @@
 
 script_path=$(dirname "$0")
 cd "$script_path" || exit
-CACHE_DIR="$HOME/.cache/nrf_sniffer"
-uv venv --clear "${CACHE_DIR}"
+VIRTUAL_ENV="$HOME/.cache/nrf_sniffer"
+uv venv --clear "${VIRTUAL_ENV}"
 uv sync
 # shellcheck disable=SC1091
-source "${CACHE_DIR}/bin/activate" || exit 1
+source "${VIRTUAL_ENV}/bin/activate" || exit 1
 uv run python "$script_path/nrf_sniffer_ble.py" "$@"
