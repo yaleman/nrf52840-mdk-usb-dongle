95e59eb2ce87f4fce6bf36d3b1712153c90055aa
James Hodgkinson <james@terminaloutcomes.com>
2026-04-18 11:32:48 +1000

    cleanup

--- tools/ble_sniffer/extcap/nrf_sniffer_ble.sh
+++ tools/ble_sniffer/extcap/nrf_sniffer_ble.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -eux
-
 script_path=$(dirname "$0")
 cd "$script_path" || exit
 export VIRTUAL_ENV="$HOME/.cache/nrf_sniffer"
-uv venv --clear "${VIRTUAL_ENV}"
+if [ ! -d "$VIRTUAL_ENV" ]; then
+    uv venv --clear "${VIRTUAL_ENV}"
+fi
 uv sync --active
-# shellcheck disable=SC1091
-# source "${VIRTUAL_ENV}/bin/activate" || exit 1
 uv run --active nrf-sniffer-ble "$@"
